58eec3dcac31e90ad5b88158f5ce8963922ac26e
Jevin Vekaria <jevin925@gmail.com>
2021-07-06 02:07:24 +0530

    added simplwjwt6

--- fungataua_backend/settings.py
+++ fungataua_backend/settings.py
@@ -142,10 +142,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-# PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 # PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-# STATIC_URL = '/static/'
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 # STATICFILES_DIRS = (
 #     os.path.join(PROJECT_ROOT, 'static'),
 # )
