fb58278f527fc4c41691a1f373f5e4aedd2eef0e
aayush1607 <auc1607@gmail.com>
2021-10-14 20:05:57 +0530

    Removed credentials

--- fungataua_backend/settings.py
+++ fungataua_backend/settings.py
@@ -103,9 +103,9 @@ WSGI_APPLICATION = 'fungataua_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db1586l0f12bqn',
-        'USER': 'herphvuzucpsqn',
-        'PASSWORD': '7d1d26ddef40eb51584ed3726658d26fdc49bbd6e6021630465bce9f88f22e32',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'ec2-34-193-101-0.compute-1.amazonaws.com',
         'PORT': 5432
     }
@@ -188,14 +188,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "fungatauatrust@gmail.com"
-EMAIL_HOST_PASSWORD = "fungataua@1234"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 
 # SECURE_SSL_REDIRECT=False
 # ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'http'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = 'AKIAR2IOJT2XD6YAZJ7X'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
 AWS_STORAGE_BUCKET_NAME = 'fungatauastorage'
 AWS_QUERYSTRING_AUTH = False
 # AWS_S3_SIGNATURE_VERSION = 's3v4'
